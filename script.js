ac556b801c1237fb84a6bd2e3e89a289940d4cc5
markzam <markzamora@live.com>
2024-06-27 19:56:03 -0600

    added js script

---
+++ script.js
@@ -0,0 +1,13 @@
+//Primitive types are Strings, Numbers and Booleans
+//Reference types are Arrays, Objects and Functions
+//alert("Hello world");
+let favorite_color = "blue";
+let age = 40;
+const name = "Mark";
+let isMinor = true;
+const favoriteColors = ["red", "blue"];
+const myBestFriendFavoriteColors = [...favoriteColors];
+favoriteColors[favoriteColors.length - 1] = "green";
+myBestFriendFavoriteColors[0] = "yellow";
+
+alert("hi");
